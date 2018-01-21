43ef0b7184963e8ef8ec365e10001787890b540d
Caroline Salib <carolinesalibc@gmail.com>
2018-01-21 12:31:24 -0200

    Fix jquery problem when try to logout in devise

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery2
+//= require jquery_ujs
 //= require popper
+//= require turbolinks
 //= require bootstrap-sprockets
+//= require_tree .
