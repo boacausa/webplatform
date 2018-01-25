564d6a4153ea11660431914e8236f978ccc1ab48
Caroline Salib <carolinesalibc@gmail.com>
2018-01-24 22:32:13 -0200

    Add custom fields on user sign-up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120141508) do
+ActiveRecord::Schema.define(version: 20180124232942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20180120141508) do
     t.string "phone"
     t.boolean "temporary_home"
     t.string "image"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
