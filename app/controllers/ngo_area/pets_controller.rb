bb577be888f7b7c6133e8a9c7ae7e963c86ed278
Caroline Salib <carolinesalibc@gmail.com>
2018-01-28 16:43:24 -0200

    Add destroy option to pets

--- app/controllers/ngo_area/pets_controller.rb
+++ app/controllers/ngo_area/pets_controller.rb
@@ -1,4 +1,6 @@
 class NgoArea::PetsController < NgoAreaController
+  before_action :set_pet, only: %i[edit update destroy]
+
   def index
     @pets = Pet.all
   end
@@ -14,17 +16,24 @@ class NgoArea::PetsController < NgoAreaController
   end
 
   def edit
-    @pet = Pet.find(params[:id])
+    #
   end
 
   def update
-    @pet = Pet.find(params[:id])
     @pet.update params_pet
 
     redirect_to ngo_area_pets_path
   end
 
+  def destroy
+    redirect_to ngo_area_pets_path if @pet.destroy!
+  end
+
   private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
 
   def params_pet
     params.require(:pet).permit(:name, :age, :sex, :description, :image, :active)
