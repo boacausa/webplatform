69f76a3acbe84821f024f1606d8530a5b4769409
Caroline Salib <carolinesalibc@gmail.com>
2018-03-03 13:11:21 -0300

    Fix error when format a null phone

--- app/services/phone_format.rb
+++ app/services/phone_format.rb
@@ -4,6 +4,7 @@ module PhoneFormat
   end
 
   def mask(phone)
+    return nil unless phone.present?
     phone.gsub!(/\D/, '')
     phone.gsub!(/^(\d{2})(\d)/, '(\1) \2')
     phone.gsub!(/(\d)(\d{4})$/, '\1-\2')
