47aa822958cddf076f372cd1f6d5511bd87fcd73
Caroline Salib <carolinesalibc@gmail.com>
2018-06-16 13:52:47 -0300

    Remove test folder

--- test/controllers/ngos_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe NgosController do
-  it "should get index" do
-    get ngos_index_url
-    value(response).must_be :success?
-  end
-
-end
--- test/models/pet_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe Pet do
-  let(:pet) { Pet.new }
-
-  it "must be valid" do
-    value(pet).must_be :valid?
-  end
-end
