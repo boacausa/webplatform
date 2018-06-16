156beb46ba7137f206ca2958239efe731d7178b2
Caroline Salib <carolinesalibc@gmail.com>
2018-06-16 17:58:46 -0300

    Add defined verification at aws initializer

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,8 @@
-Aws.config.update(
-  region: 'us-west-2',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-)
+if defined?(Aws)
+  Aws.config.update(
+    region: 'us-west-2',
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  )
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+  S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+end
