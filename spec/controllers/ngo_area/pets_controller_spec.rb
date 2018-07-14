aeba7c8b285d07d67ca3d3a0bce43a16a1c8d453
Caroline Salib <carolinesalibc@gmail.com>
2018-07-13 21:02:09 -0300

    Add factory to

--- spec/controllers/ngo_area/pets_controller_spec.rb
+++ spec/controllers/ngo_area/pets_controller_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
-RSpec.describe NgoArea::PetsController, type: :controller do
+describe NgoArea::PetsController do
+  login_user
 
+  subject { create(:pet) }
+
+  describe 'GET #index' do
+    before do
+      get :index
+    end
+
+    it 'returns 200 code status' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET #new' do
+    before do
+      get :new
+    end
+
+    it 'returns 200 code status' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET #create' do
+    before do
+      get :create, params: { pet: attributes_for(:pet) }
+    end
+
+    it 'returns 302 code status' do
+      expect(response).to have_http_status(:found)
+    end
+
+    it 'redirects to index' do
+      expect(response).to redirect_to(action: :index)
+    end
+  end
+
+  describe 'GET #edit' do
+    before do
+      get :edit, params: { id: subject.id }
+    end
+
+    it 'returns 200 code status' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET #update' do
+    before do
+      get :update, params: { id: subject.id, pet: attributes_for(:pet) }
+    end
+
+    it 'returns 302 code status' do
+      expect(response).to have_http_status(:found)
+    end
+
+    it 'redirects to index' do
+      expect(response).to redirect_to(action: :index)
+    end
+  end
 end
