0032ebc020d6ad9be75e5514c475d4fab043bf34
Caroline Salib <carolinesalibc@gmail.com>
2018-07-13 22:40:00 -0300

    Add enum with group permissions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   include PhoneFormat
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  enum group: [:admin, :ngo]
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   def phone=(text)
     super(only_numbers text)
