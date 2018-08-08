3729df014057949224ec6796521fec5c0f294db7
Caroline Salib <carolinesalibc@gmail.com>
2018-08-07 23:06:05 -0300

    Add phone mask to user register

---
+++ app/assets/javascripts/ngo_area/users/form.js
@@ -0,0 +1,19 @@
+var phone_number = document.getElementById("user_phone");
+phone_number.onkeyup = mask;
+
+function mask(){
+    setTimeout("execmask()",1);
+}
+
+function execmask(){
+    phone_number.value=mtel(phone_number.value);
+}
+
+function mtel(v){
+    v=v.replace(/\D/g,"");
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2");
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2");
+    return v;
+}
+
+execmask();
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w[ngo_area.css ngo_area.js ngo_area/ngos/form.js]
+Rails.application.config.assets.precompile += %w[ngo_area.css ngo_area.js ngo_area/ngos/form.js ngo_area/users/form.js]
