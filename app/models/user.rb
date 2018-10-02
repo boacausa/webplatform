e442e245595fd0803ecb2381e4966fd601478da8
Caroline Salib <carolinesalibc@gmail.com>
2018-10-02 20:30:42 -0300

    Fix google authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,11 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :ngos
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
+  def self.from_omniauth(auth)
+    user = User.find_or_create_by(email: auth.info['email'])
+    user.name = auth.info['name']
+    user.password = Devise.friendly_token[0, 20]
 
-    unless user
-        user = User.create(name: data['name'],
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
-        )
-    end
     user
   end
 
