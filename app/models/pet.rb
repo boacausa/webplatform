8601e4004d23e0aeb552d184a3855dd60b896eb7
Caroline Salib <carolinesalibc@gmail.com>
2018-10-11 21:48:42 -0300

    Add presence validation to sex and ngo on pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -8,6 +8,7 @@ class Pet < ApplicationRecord
 
   has_attached_file :image, styles: { medium: '320x320#', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :name, :sex, :ngo, presence: true
 
   scope :actived, -> { where(active: true) }
 
