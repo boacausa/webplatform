966015c4f763b8dd6763fefafb3ce6fe99429a87
Caroline Salib <carolinesalibc@gmail.com>
2018-12-26 20:31:38 -0200

    Add SMTP configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,14 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "uberdo3setor_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => ENV['HOSTNAME'] }
+  config.action_mailer.smtp_settings = {
+      port: ENV['SMTP_PORT'].to_i,
+      address: ENV['SMTP_HOST'],
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: ENV['SMTP_USERNAME'],
+      password: ENV['SMTP_PASSWORD']
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
