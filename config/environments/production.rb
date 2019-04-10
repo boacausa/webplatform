404d4eb339c48b0e1ac71750d84e1c3fbf6eef53
Caroline Salib <carolinesalibc@gmail.com>
2019-04-09 18:42:29 -0600

    Change config from paperclip s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,9 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_region: ENV['AWS_REGION'],
+    preserve_files: true,
     s3_credentials: {
+      s3_region: ENV['AWS_REGION'],
       bucket: ENV['AWS_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
