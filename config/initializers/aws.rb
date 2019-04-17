6243689cd93825948c75c2ab55b6211d069d8c09
Caroline Salib <carolinesalibc@gmail.com>
2019-04-16 18:51:33 -0600

    Remove aws initializer

--- config/initializers/aws.rb
+++
@@ -1,8 +0,0 @@
-if defined?(Aws) && Rails.env.production?
-  Aws.config.update(
-    region: 'us-west-2',
-    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-  )
-
-  S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
-end
