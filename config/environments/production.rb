a11ac89f92ebf400019e48ed7e3d485690147ded
Caroline Salib <carolinesalibc@gmail.com>
2019-04-18 18:16:09 -0600

    Fix wrong name to active storage service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.active_storage.service = :production
+  config.active_storage.service = :amazon
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
