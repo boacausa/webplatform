aaedda285bd107f4b6519aea77ea30b3a1788b4a
Caroline Salib <carolinesalibc@gmail.com>
2019-07-16 08:07:00 -0600

    Reinstall webpack and configure css modules

--- app/javascript/containers/NgosList.jsx
+++ app/javascript/containers/NgosList.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import Masks from "../utils/Masks";
 import {Link} from "react-router-dom";
+import TestCssStuff from "../packs/test_css_stuff";
 
 const GET_NGOS_REQUEST = 'GET_NGOS_REQUEST';
 const GET_NGOS_SUCCESS = 'GET_NGOS_SUCCESS';
@@ -65,6 +66,7 @@ class NgosList extends React.Component {
 
         return (
             <div className="container">
+                <TestCssStuff/>
                 <div className="jumbotron">
                     <h1 className="display-4">Conheça as ONGs que fazem parte</h1>
                     <p className="lead">Encontre aqui uma ONG para acompanhar suas atividades, consultar sua situação financeira e fazer doações.</p>
---
+++ app/javascript/packs/test_css_stuff.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as styles from '../hello_react/styles/test-css-stuff.module.css'
+
+const TestCssStuff = () => (
+    <div className={styles.testCssStuff}>
+        <p>Test Css Modules!</p>
+    </div>
+);
+
+export default TestCssStuff;
