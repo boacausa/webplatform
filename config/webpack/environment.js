475d5744f7512b07ab2f9f45e154862596dea666
Caroline Salib <carolinesalibc@gmail.com>
2019-07-29 07:06:52 -0600

    Try to override loader options
    https://github.com/rails/webpacker/blob/master/docs/webpack.md#overriding-loader-options-in-webpack-3-for-css-modules-etc

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const merge = require('webpack-merge')
+
+const myCssLoaderOptions = {
+    modules: true,
+    sourceMap: true,
+    localIdentName: '[name]__[local]___[hash:base64:5]'
+}
+
+const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === 'css-loader')
+
+CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions)
 
 module.exports = environment
