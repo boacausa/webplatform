3ef01f63fc987e28ddaf0640cc7da715141e0268
Caroline Salib <carolinesalibc@gmail.com>
2019-08-06 07:28:27 -0600

    Create a SimpleSubmitButton component
    To be used as a generic submit button

--- app/javascript/containers/AdoptionList/AdoptionList.jsx
+++ app/javascript/containers/AdoptionList/AdoptionList.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import SimpleHeaderText from "../../components/SimpleHeaderText/SimpleHeaderText";
 import styles from './AdoptionList.sass'
+import SimpleSubmitButton from "../../components/SimpleSubmitButton/SimpleSubmitButton";
 
 class AdoptionList extends React.Component {
     render() {
@@ -11,7 +12,13 @@ class AdoptionList extends React.Component {
                     subtitle='Encontre aqui os animais disponíveis para adoção. Clique em "Adote" para saber mais.'
                 />
                 <div className={styles.filter}>
-
+                    <form>
+                        <label>
+                            Cidade
+                            <input type="text" name="name" />
+                        </label>
+                        <SimpleSubmitButton name='Procurar' />
+                    </form>
                 </div>
             </div>
         );
