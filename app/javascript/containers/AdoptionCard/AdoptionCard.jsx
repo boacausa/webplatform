688eb7752281eb4ea6eb0d20df88ebfe5597c985
Caroline Salib <carolinesalibc@gmail.com>
2019-08-26 07:02:28 -0600

    Fix indentation for js class

--- app/javascript/containers/AdoptionCard/AdoptionCard.jsx
+++ app/javascript/containers/AdoptionCard/AdoptionCard.jsx
@@ -4,36 +4,36 @@ import SimpleCircularButton from "../../components/SimpleCircularButton/SimpleCi
 
 const AdoptionCard = () => {
     return (
-      <div className={styles.AdoptionCard}>
-          <div className={styles.sexLabel}><a>Femea</a></div>
-          <div className={styles.ageLabel}><a>8 anos</a></div>
-        <div className={styles.picture}>
+        <div className={styles.AdoptionCard}>
+            <div className={styles.sexLabel}><a>Femea</a></div>
+            <div className={styles.ageLabel}><a>8 anos</a></div>
+            <div className={styles.picture}>
 
-        </div>
-        <div className={styles.cardContent}>
-            <a className={styles.city}>Içara, SC</a>
-            <a className={styles.petName}>Marquinhos</a>
-            <a className={styles.petDescription}>
-                Marquinhos é um cachorro muito brincalhão,
-                adora comer e rolar na grama. As vezes faz xixi
-                no lugar errado, mas ele ainda é neném e vai
-                aprender.
-            </a>
-            <div className={styles.postDetail}>
-                <div className={styles.ngoDetail}>
-                    <div className={styles.ngoPicture}>
+            </div>
+            <div className={styles.cardContent}>
+                <a className={styles.city}>Içara, SC</a>
+                <a className={styles.petName}>Marquinhos</a>
+                <a className={styles.petDescription}>
+                    Marquinhos é um cachorro muito brincalhão,
+                    adora comer e rolar na grama. As vezes faz xixi
+                    no lugar errado, mas ele ainda é neném e vai
+                    aprender.
+                </a>
+                <div className={styles.postDetail}>
+                    <div className={styles.ngoDetail}>
+                        <div className={styles.ngoPicture}>
+                        </div>
+                        <a className={styles.ngoName}>ONG Amigo Bicho</a>
                     </div>
-                    <a className={styles.ngoName}>ONG Amigo Bicho</a>
-                </div>
-                <div>
-                    <a className={styles.postTime}>4 mêses atrás</a>
+                    <div>
+                        <a className={styles.postTime}>4 mêses atrás</a>
+                    </div>
                 </div>
             </div>
+            <div className={styles.adoptionButton}>
+                <SimpleCircularButton name='Adote'/>
+            </div>
         </div>
-        <div className={styles.adoptionButton}>
-            <SimpleCircularButton name='Adote' />
-        </div>
-      </div>
     );
 };
 
