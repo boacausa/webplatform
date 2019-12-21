4c374cc737477ed71f6dee8605a46d3b8bbfa7af
Caroline Salib <carolinesalibc@gmail.com>
2019-12-21 10:42:06 -0700

    Notify all admins when there is a new adoption interest notification
    #113

--- app/jobs/notify_adoption_interest.rb
+++ app/jobs/notify_adoption_interest.rb
@@ -2,7 +2,8 @@ class NotifyAdoptionInterest < ApplicationJob
   queue_as :default
 
   def perform(adoption_interest)
-    adoption_interest.pet.ngo.users.each do |user|
+    users = User.admin + adoption_interest.pet.ngo.users
+    users.uniq.each do |user|
       AdoptionInterestNotification.create(user: user, adoption_interest: adoption_interest)
     end
   end
--- spec/jobs/notify_adoption_interest_spec.rb
+++ spec/jobs/notify_adoption_interest_spec.rb
@@ -19,8 +19,6 @@ describe NotifyAdoptionInterest do
 
       ngo_user1.ngos << ngo1
       ngo_user2.ngos << ngo1
-      ngo_user3.ngos << ngo1
-      ngo_user4.ngos << ngo2
 
       pet1 = create(:pet, ngo: ngo1)
       _pet2 = create(:pet, ngo: ngo2)
@@ -28,10 +26,11 @@ describe NotifyAdoptionInterest do
       adoption_interest = create(:adoption_interest, user: user1, pet: pet1)
 
       expect(AdoptionInterestNotification.where(user: [ngo_user1, ngo_user2, ngo_user3], adoption_interest: adoption_interest).count).to eq(0)
-      expect { described_class.perform_now(adoption_interest) }.to change { AdoptionInterestNotification.count }.by(3)
+      expect { described_class.perform_now(adoption_interest) }.to change { AdoptionInterestNotification.count }.by(4)
       expect(AdoptionInterestNotification.where(user: ngo_user1, adoption_interest: adoption_interest).count).to eq(1)
       expect(AdoptionInterestNotification.where(user: ngo_user2, adoption_interest: adoption_interest).count).to eq(1)
       expect(AdoptionInterestNotification.where(user: ngo_user3, adoption_interest: adoption_interest).count).to eq(1)
+      expect(AdoptionInterestNotification.where(user: ngo_user4, adoption_interest: adoption_interest).count).to eq(1)
     end
   end
 end
