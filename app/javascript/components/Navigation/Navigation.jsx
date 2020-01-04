d9b633c371cc4f20ba9974169aee67140240798e
Caroline Salib <carolinesalibc@gmail.com>
2020-01-04 07:59:16 -0700

    Open user menu dropdown when clicking on more button
    #99

--- app/javascript/components/Navigation/Navigation.jsx
+++ app/javascript/components/Navigation/Navigation.jsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import styles from './Navigation.sass';
 import {NavLink} from "react-router-dom";
+import UserMenu from "./UserMenu/UserMenu";
 
-const Navigation = (props) => {
-    function loginButton() {
+class Navigation extends React.Component {
+    state = {
+        userMenuActive: false
+    };
+
+    loginButton = () => {
         return <a href="/users/sign_in" className={styles.loginButton}>Entrar</a>;
-    }
+    };
 
-    function userButtons() {
-        // TODO: Link to admin panel
-        // TODO: Link to edit user
+    toggleMoreButtonHandler = () => {
+        this.setState({userMenuActive: !this.state.userMenuActive});
+    };
 
+    userButtons = () => {
         return <div className={styles.userProperties}>
             <span className={styles.userPicture} />
-            <a className={styles.moreButton} />
+            <button className={styles.moreButton} onClick={() => this.toggleMoreButtonHandler()}>
+                <i className={styles.moreIcon} aria-hidden="true" />
+            </button>
+            {this.state.userMenuActive && <UserMenu />}
         </div>;
-    }
+    };
 
-    return <nav className={styles.Navigation}>
-        <h1 className={styles.title}>Boa Causa</h1>
-        <div className={styles.links}>
-            <NavLink exact className={styles.link} activeClassName={styles.linkActive} to="/">Home</NavLink>
-            <NavLink className={styles.link} activeClassName={styles.linkActive} to="/ongs">ONGs</NavLink>
-            <NavLink className={styles.link} activeClassName={styles.linkActive} to="/adocao">Adoção</NavLink>
-        </div>
-        {props.userEmail ? userButtons() : loginButton()}
-    </nav>;
-};
+    render() {
+        return (
+            <nav className={styles.Navigation}>
+                <h1 className={styles.title}>Boa Causa</h1>
+                <div className={styles.links}>
+                    <NavLink exact className={styles.link} activeClassName={styles.linkActive} to="/">Home</NavLink>
+                    <NavLink className={styles.link} activeClassName={styles.linkActive} to="/ongs">ONGs</NavLink>
+                    <NavLink className={styles.link} activeClassName={styles.linkActive} to="/adocao">Adoção</NavLink>
+                </div>
+                {this.props.userEmail ? this.userButtons() : this.loginButton()}
+            </nav>
+        );
+    };
+}
 
 export default Navigation;
---
+++ app/javascript/components/Navigation/UserMenu/UserMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./UserMenu.sass";
+
+const UserMenu = () => {
+    // TODO: Link to admin panel
+    // TODO: Link to edit user
+    return <ul className={styles.UserMenu}>
+        <li className={styles.menuRow}>Configurações</li>
+    </ul>
+};
+
+export default UserMenu;
