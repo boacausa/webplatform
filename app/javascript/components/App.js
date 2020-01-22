1fdeb9ef20a8edaf1f1405770bff5d32186b5393
Caroline Salib <carolinesalibc@gmail.com>
2020-01-22 08:03:04 -0700

    Use new style on side menu
    #99

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import {Provider} from 'react-redux';
 import configureStore from '../configureStore';
 import NgosList from "../containers/NgosList";
