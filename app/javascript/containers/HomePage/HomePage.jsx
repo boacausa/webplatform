2d70ba4e359e1ff9094d7055a27f156260aede80
Caroline Salib <carolinesalibc@gmail.com>
2020-02-18 06:54:56 -0700

    Add home page initial box skeleton
    #98

--- app/javascript/containers/HomePage/HomePage.jsx
+++ app/javascript/containers/HomePage/HomePage.jsx
@@ -1,14 +1,28 @@
 import React, {Component} from "react"
-import styles from './HomePage.sass'
+import classes from './HomePage.sass'
 
 class HomePage extends Component {
     render() {
+
         return (
-            <div className={styles.HomePage}>
-                Test
+            <div className={classes.HomePage}>
+                <div className={classes.initialBox} style={styles.initialBoxHeight}>
+                    <div className={classes.insideBorder}>
+                        <div className={classes.titleBox}>
+                            <h1 className={classes.title}>Por uma boa causa</h1>
+                            <h2 className={classes.subtitle}> Para fazer do mundo um lugar melhor para todos.</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
+const styles = {
+  initialBoxHeight: {
+      height: (window.innerHeight * 0.75) + "px"
+  }
+};
 
 export default HomePage;
