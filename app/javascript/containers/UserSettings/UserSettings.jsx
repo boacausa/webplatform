f9429d3dcb6b5db751cdefcf03db1d9ac01a87c6
Caroline Salib <carolinesalibc@gmail.com>
2020-03-31 08:05:43 -0600

    Add second level navigation for user configurations
    #134

--- app/javascript/containers/UserSettings/UserSettings.jsx
+++ app/javascript/containers/UserSettings/UserSettings.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import classes from './UserSettings.sass'
+import SecondLevelNavigation from "../../components/Navigation/SecondLevelNavigation/SecondLevelNavigation";
 
 const UserSettings = () => {
     return (
         <div className={classes.UserSettings}>
-            User Settings
+            <SecondLevelNavigation>
+                <p className={classes.configurationMenu}>Configurações</p>
+                <span className={classes.configurationMenuSelected} />
+            </SecondLevelNavigation>
         </div>
     );
 };
