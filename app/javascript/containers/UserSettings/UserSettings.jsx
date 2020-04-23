afb5dd3a80a16bf5b3617407ee887e4e2b6c1817
Caroline Salib <carolinesalibc@gmail.com>
2020-04-23 08:11:51 -0600

    Load user data from redux
    #134

--- app/javascript/containers/UserSettings/UserSettings.jsx
+++ app/javascript/containers/UserSettings/UserSettings.jsx
@@ -1,9 +1,36 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import classes from './UserSettings.sass'
 import SecondLevelNavigation from "../../components/Navigation/SecondLevelNavigation/SecondLevelNavigation";
 import TextInputWithLabel from "../../components/TextInputWithLabel/TextInputWithLabel";
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
 
-const UserSettings = () => {
+const UserSettings = (props) => {
+    const [userSettings, setUserSettings] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        password: '**********',
+        passwordConfirmation: '**********'
+    });
+
+    useEffect(() => {
+        const user = props.user;
+
+        const userData = {
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+        };
+
+        setUserSettings(userData)
+
+        return () => {
+            // cleanup
+        };
+    }, []);
+
+
     return (
         <div className={classes.UserSettings}>
             <SecondLevelNavigation>
@@ -18,13 +45,33 @@ const UserSettings = () => {
 
                 <div className={classes.formInputs}>
                     <div>
-                        <TextInputWithLabel className={classes.textInput} label="Nome Completo" />
-                        <TextInputWithLabel className={classes.textInput} label="Email" />
-                        <TextInputWithLabel className={classes.textInput} label="Telefone" />
+                        <TextInputWithLabel
+                            className={classes.textInput}
+                            label="Nome Completo"
+                            value={userSettings.name}
+                         />
+                        <TextInputWithLabel
+                            className={classes.textInput}
+                            label="Email"
+                            value={userSettings.email}
+                        />
+                        <TextInputWithLabel
+                            className={classes.textInput}
+                            label="Telefone"
+                            value={userSettings.phone}
+                        />
                     </div>
                     <div>
-                        <TextInputWithLabel className={classes.textInput} label="Senha" />
-                        <TextInputWithLabel className={classes.textInput} label="Confirmação de senha" />
+                        <TextInputWithLabel
+                            className={classes.textInput}
+                            label="Senha"
+                            value={userSettings.password}
+                        />
+                        <TextInputWithLabel
+                            className={classes.textInput}
+                            label="Confirmação de senha"
+                            value={userSettings.passwordConfirmation}
+                        />
                     </div>
                 </div>
             </div>
@@ -32,4 +79,8 @@ const UserSettings = () => {
     );
 };
 
-export default UserSettings;
+const mapStateToProps = createStructuredSelector({
+    user: state => state.app.user
+});
+
+export default connect(mapStateToProps, null)(UserSettings);
