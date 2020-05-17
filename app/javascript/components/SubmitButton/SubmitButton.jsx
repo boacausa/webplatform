b307a810413853ac879eb7f9886d73720e13d6c3
Caroline Salib <carolinesalibc@gmail.com>
2020-05-17 17:14:02 -0600

    Force js test error
    #134

--- app/javascript/components/SubmitButton/SubmitButton.jsx
+++ app/javascript/components/SubmitButton/SubmitButton.jsx
@@ -9,7 +9,6 @@ const SubmitButton = (props) => {
         <button
             onClick={props.clicked}
             className={cx(buttonClass, props.classStyleModifier)}
-            disabled={props.isDisabled}
         >{props.title}</button>
     );
 };
