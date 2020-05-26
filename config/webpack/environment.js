f3de0f13ac0725693b36f0bee226fbd0828e9229
Caroline Salib <carolinesalibc@gmail.com>
2020-05-26 08:50:46 -0600

    Do not compile test.js files
    This was breaking heroku deploy because of missing [dev] dependencies

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -12,5 +12,8 @@ const CSSLoader = environment.loaders.get('sass').use.find(el => el.loader === '
 CSSLoader.options = merge(CSSLoader.options, myCssLoaderOptions);
 
 environment.loaders.get('file').test = /\.(woff|woff2|eot|ttf|svg)$/
+// Do not compile test files (this will cause prod compiler to crash because it does not recognize dev dependencies)
+environment.loaders.get('babel').exclude = [/\.test\.js$/, environment.loaders.get('babel').exclude]
+
 
 module.exports = environment;
